54509e94e103ab1988f306e65072235f8d7e5b05
Mason3144 <lts890303@gmail.com>
2022-06-26 16:18:53 +0900

    State, Props, Effects

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("I run all the time");
+
+  useEffect(() => console.log("I run once"), []);
+  useEffect(() => {
+    if (keyword !== "") {
+      console.log("I run when keyword changes");
+    }
+  }, [keyword]);
   return (
     <div>
-      <h1 className={styles.title}>Hello world!</h1>
-      <Button text="Continue" />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Serch here..."
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click</button>
     </div>
   );
 }
